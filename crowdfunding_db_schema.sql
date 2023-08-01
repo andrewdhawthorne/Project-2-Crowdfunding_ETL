774ee6ba54d015c9407cc57d5db65a7d8dbdee32
andrewdhawthorne <adhawthorne@gmail.com>
2023-07-31 19:13:27 -0500

    updating schema and uploading campaign screenshot

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,27 +1,3 @@
--- Create campaign table and import csv file
-CREATE TABLE campaign (
-	cf_id INT PRIMARY KEY,
-	contact_id INT, 
-	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
-	company_name VARCHAR(255),
-	description VARCHAR(255),
-	goal INT,
-	pledged INT, 
-	outcome CHAR(255),
-	backers_count INT, 
-	country CHAR(255),
-	currency CHAR(255),
-	launch_date DATE, 
-	end_date DATE, 
-	category_id VARCHAR(255),
-	FOREIGN KEY (category_id) REFERENCES category (category_id),
-	subcategory_id VARCHAR(255),
-	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
-); 
-
--- Query all fields from the table
-SELECT * FROM campaign;
-
 -- Create category table and import csv file
 CREATE TABLE category (
 	category_id VARCHAR(255) PRIMARY KEY,
@@ -31,7 +7,7 @@ CREATE TABLE category (
 -- Query all fields from the table
 SELECT * FROM category;
 
--- Create subcategory table and import csv file 
+-- Create subcategory table and import csv file
 CREATE TABLE subcategory (
 	subcategory_id VARCHAR(255) PRIMARY KEY,
 	subcategory CHAR(255)
@@ -50,3 +26,27 @@ CREATE TABLE contacts (
 
 -- Query all fields from the table
 SELECT * FROM contacts;
+
+-- Create campaign table and import csv file
+CREATE TABLE campaign (
+	cf_id INT PRIMARY KEY,
+	contact_id INT,
+	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+	company_name VARCHAR(255),
+	description VARCHAR(255),
+	goal FLOAT,
+	pledged FLOAT,
+	outcome VARCHAR(255),
+	backers_count INT,
+	country VARCHAR(255),
+	currency VARCHAR(255),
+	launch_date DATE,
+	end_date DATE,
+	category_id VARCHAR(255),
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	subcategory_id VARCHAR(255),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
+);
+
+-- Query all fields from the table
+SELECT * FROM campaign;
